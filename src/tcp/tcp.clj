10a6e86f6ef6cd8ae13322bce397fa5e051e8a82
Matthew Gordon <mgordon@palantir.com>
2014-12-27 17:39:03 -0600

    Modified the map to contain flag sizes and flattened it to make it easier to use

--- src/tcp/tcp.clj
+++ src/tcp/tcp.clj
@@ -5,35 +5,24 @@
       )
 
   )
-
+(defn make-offset [word-offset byte-offset size] {:word-offset word-offset :byte-offset byte-offset :size size})
 (def header-template ; header is represented as a vector of 32 bit words
   ; each value in each hash is an offset into the word for that value
   ; e.g. :flags is offset 10 bits into the 4th word
-  [
     {
-      :source-port 0
-      :destination-port 16
+      :source-port (make-offset 0 0 16)
+      :destination-port (make-offset 0 16 16)
+      :seq-num (make-offset 1 0 32)
+      :ack-num (make-offset 2 0 32)
+      :data-offset (make-offset 3 0 4)
+      ; there's a gap here for reserved space
+      :flags (make-offset 3 10 6) 
+      :window (make-offset 3 16 16)
+      :checksum (make-offset 4 0 16)
+      :urg (make-offset 4 16 16)
+      :options (make-offset 5 0 32)
     }
-    {
-      :seq-num 0 
-    }; using an arbitrary fixed initial sequence number for simplicity in this case
-    {
-      :ack-num 0
-    }
-    {
-      :data-offset 0
-      :flags 10
-      :window 16
-    }
-    {
-      :checksum 0
-      :urg 16
-    }
-    {
-      :options 0
-    }
-    ; data goes here
-  ]
+    ; data goes here after word 5
 )
 
 (defn set-header-value [flag value header]
@@ -46,12 +35,8 @@
   "
   ; pull apart the header template to get the word offset and byte offset for the flag in question
   (let [
-          [word-offset byte-offset]
-          (->> tcp/header-template 
-            (map-indexed vector) 
-            (filter #(contains? (% 1) flag))
-            (first)
-          )
+          {word-offset :word-offset byte-offset :byte-offset size :size} 
+          (tcp/header-template flag)
         ]
      
   )
